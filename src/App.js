0c41f68479575b75023fed000d1890e0dcf3d330
ale-cristofori <compatt84@gmail.com>
2018-12-06 10:44:25 +0000

    fixed styleName attributes to classNames

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div styleName="App">
-        <header styleName="App-header">
-          <img src={logo} styleName="App-logo" alt="logo" />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            styleName="App-link"
+            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -26,3 +26,4 @@ class App extends Component {
 }
 
 export default App;
+
