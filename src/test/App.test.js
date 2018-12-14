f1b3176340c2053d84593da4b5d4121e3e7a5076
ale-cristofori <compatt84@gmail.com>
2018-12-14 23:33:19 +0000

    finished testing on map component

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -10,16 +10,12 @@ configure({ adapter: new Adapter() });
 
 describe('App Component testing', function() {
 
-  it('App renders main message', () => {
-    const wrapper = mount(<App />);
-    const editText = <select>
-                      <option value="-14198.801050682045,6711510.640113423" class="dd-locations">london</option>
-                      <option value="4187594.340763207,7509144.108588474" class="dd-locations">moscow</option>
-                      <option value="3225415.454040626,5014229.844289909" class="dd-locations">istanbul</option>
-                      <option value="1391493.6349159197,5146011.679282788" class="dd-locations">rome</option>
-                      <option value="828862.6645485563,5933916.615134273" class="dd-locations">bern</option>
-                    </select>;
-    expect(wrapper).to.contain(editText);
+  it('App drop down input', () => {
+    const selectWrapper = mount(<App />).find('select');
+    const editText = '<select><option value="-14198.801050682045,6711510.640113423" class="dd-locations">London</option><option value="4187594.340763207,7509144.108588474" class="dd-locations">Moscow</option><option value="3225415.454040626,5014229.844289909" class="dd-locations">Istanbul</option><option value="1391493.6349159197,5146011.679282788" class="dd-locations">Rome</option><option value="828862.6645485563,5933916.615134273" class="dd-locations">Bern</option></select>';
+    expect(selectWrapper.html()).to.equal(editText);
+    //selectWrapper.simulate('change', {target: { value : "3225415.454040626,5014229.844289909"}});
+    //expect(selectWrapper.props().value).to.be("3225415.454040626,5014229.844289909");
 });
 
   chai.use(chaiEnzyme())
